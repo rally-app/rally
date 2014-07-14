b9319a34f5e2061d499979bb071d3e788eb2f5b7
jaredscheib <jared.scheib@gmail.com>
2014-07-14 12:53:03 -0700

    Implement near-complete VoteOptionsView with mustache templating and events

--- public/scripts/models/planModel.js
+++ public/scripts/models/planModel.js
@@ -6,18 +6,19 @@ var PlanModel = Backbone.Model.extend({
   // hostWhen: new Date(),
   // hostWhat: 'Drink',
   // hostWhere: 'Tenderloin',
-//   createdAt: new Date(),
-//   rounds: [
-//     {
-//       options: [
-//         {
-//           optionName: String,
-//           votes: Number
-//         }, {}, {}
-//       ],
-//       "winner": String // starts null
-//     }, {}, {}
-//   ]
+  // createdAt: new Date(),
+  // rounds: [
+  //   {
+  //     options: [
+  //       {
+  //         optionName: String,
+  //         votes: Number
+  //       }, {}, {}
+  //     ],
+  //     "winner": String // starts null
+  //   }, {}, {}
+  // ],
+  // currentRound: 1
 // }
 
 
--- public/scripts/views/VoteOptionsView.js
+++ public/scripts/views/VoteOptionsView.js
@@ -1,49 +1,60 @@
 var VoteOptionsView = Backbone.View.extend(){
 
+// var VoteModel = Backbone.Model.extend({
+//   planId: String,
+//   userVotes: []
+// });
+
   tagName: 'div',
   
   template: ['<span id="affirmation">Great! Where to?</span>
               <div id="options">
-                <div id="rec1-container">
-                  <input id="priority" type="button" value="Option 1" onclick="setPriority(1)">
-                </div>
-                <div id="rec2-container">
-                  <input id="priority" type="button" value="Option 2" onclick="setPriority(2)">
-                </div>
-                <div id="rec3-container">
-                  <input id="priority" type="button" value="Option 3" onclick="setPriority(3)">
-                </div>
+                <input class="priority" type="button" value="{{ #rounds[currentRound].options }}{{ optionName }}{{ /rounds[currentRound].options }}">
               </div>
               <div id="vote">
-                <input id="submitVote" type="button" value="Vote!" onclick="submitVote()">
+                <input id="submitVote" type="button" value="Vote!">
               </div>
               <div id="counters">
                 <span id="rally-eta">{{ ??? }}</span>
                 <span id="round-number">{{ currentRound + '/' + rounds.length }}</span>
                 <span id="round-deadline">{{ ??? }}</span>
-              </div>
-
-
-
-              '].join(''),
+              </div>'].join(''),
 
   initialize: function(){
-
+    this.render();
   },
 
   events: {
-    // 'change': 
+    'click .priority': 'controller.setPriority',
+    'click #submitVote': 'controller.submitVote' 
   },
 
   render: function(){
+    this.$el.children.detach();
+    this.$el.html(this.template.compile(this.model.attributes));
     return $el;
   },
 
   controller: {
+    setPriority: function(num){
+      var userVotes = this.model.get('userVotes');
+      uservotes.push(/* find element that called event, optionName or index */);
+      this.model.set('userVotes', userVotes);
+      if( userVotes.length === this.model.get('rounds').length ){
+        this.model.trigger('click #submitVote');
+      }
+    },
+
+    submitVote: function(){
+      this.model.save();
+    }
 
   }
 
 };
+
+// questions
+// localRound - stored in local storage, instead of rounds[0]
 
                   // <span id="rec1-title">{{ title }}</span>
                   // <span id="rec1-description"></span>
