d6136bdad24d6f20b5bd9642a2d42a633ba2fa07
Michael Schippert <mSchip@Michaels-MacBook-Pro-4.local>
2014-07-14 15:48:39 -0700

    Modify buildPlanView, proposedPlanView, and router

--- public/scripts/routers/router.js
+++ public/scripts/routers/router.js
@@ -3,7 +3,9 @@ Router = Backbone.Router.extend({
   Routes: {
     "/": "buildPlan",
     "/:id": "proposedPlan",
-    "/:id/round/:roundNum": "voteOptionsView"
+    "/:id/round/:roundNum": "voteOptionsView",
+    "/:id/round/:roundNum/voted": "voteConfirmed",
+    "/:id/result": "finalizedPlanView"
   },
 
   buildPlan: function() {
@@ -12,6 +14,7 @@ Router = Backbone.Router.extend({
   },
 
   //Renders a view for invitees to accept the invitaion to rally
+  //TODO: will need some session logic to redirect to either vote options or vote confirmed if user has voted on this round.
   proposedPlan: function( id ) {
     var planModel = new PlanModel();
     planModel.fetch( id: id );
@@ -19,10 +22,23 @@ Router = Backbone.Router.extend({
   },
 
   //Renders a view for the first round of voting
-  voteOptions: function(id, round) {
+  //TODO: will need some session logic to redirect to vote confirmed if user has voted on this round.
+  voteOptions: function( id, round ) {
     var planModel = new PlanModel();
     planModel.fetch( id: id );
     var voteOptionsView = new VoteOptionsView({ model: planModel });
+  },
+
+  //Renders the voteComfirmed view to indicate a state of waiting for group decision.
+  voteConfirmed: function() {
+    var planModel = new PlanModel();
+    var voteConfirmedView = new VoteConfirmedView( model: planModel );
+  },
+
+  finalizedPlanView: function( id ) {
+    var planModel = new PlandModel();
+    planModel.fetch( id: id );
+    var finalizedPlanView = new FinalizedPlanView({ model: planModel });
   }
   
 })
--- public/scripts/views/buildPlanView.js
+++ public/scripts/views/buildPlanView.js
@@ -1,8 +1,23 @@
 // var Hogan = require( "hogan" );
+var hogan = window.Hogan;
 
 window.BuildPlanView = Backbone.View.extend({
 
-  template: '<div class="buildPlan"><p>I want to <input type="text" name="hostWhat" placeholder="eat/drink" value="drink"></input>near <input type="text" name="hostWhere" placeholder="location" value="downtown sf"></input> at <input type="text" name="hostWhen" placeholder="time" value="today 7:00pm"></input> with <input type="text" name="hostWho" placeholder="these people" value="Nick, Jared"></input>.</p><button type="button" class="createEvent">Check Mark Image</button><button type="reset" class="clear">Clear</button></div>',
+  template: hogan.complile(['<div class="buildPlan">',
+    '<p>',
+      '<input type="text" name="hostName" placeholder="Full Name"></input> want to ',
+      '<select name="hostWhat">',
+        '<option value="drink">drink</option>',
+        '<option value="eat">eat</option>',
+      '</select> near ',
+      '<input type="text" name="hostWhere" placeholder="location"></input> at ',
+      '<input type="time" name="hostWhen"></input> with ',
+      '<input type="text" name="hostWho" placeholder="these people" value="Nick, Jared"></input>.',
+    '</p>',
+    '<button type="button" class="createEvent">Check Mark Image</button>',
+    '<button type="reset" class="clear">Clear</button></div>'].join("")),
+
+
 
   initialize: function() {
     this.render();
@@ -18,20 +33,25 @@ window.BuildPlanView = Backbone.View.extend({
     e && e.preventDefault();
 
     //Sets the planModel host values equal to the form inputs
-    this.model.set('hostWhat', this.$el.find('[name="hostWhat"]').val());
-    this.model.set('hostWhere', this.$el.find('[name="hostWhere"]').val());
-    this.model.set('hostWhen', this.$el.find('[name="hostWhen"]').val());
-    this.model.set('hostWho', this.$el.find('[name="hostWho"]').val());
+    this.model.set( 'hostWho', parseInvites(this.$el.find('[name="hostWho"]' ).val()));
+    this.model.set( 'hostName', this.$el.find('[name="hostName"]' ).val());
+    this.model.set( 'hostWhat', this.$el.find('[name="hostWhat"]' ).val());
+    this.model.set( 'hostWhere', this.$el.find('[name="hostWhere"]' ).val());
+    this.model.set( 'hostWhen', this.$el.find('[name="hostWhen"]' ).val());
 
     //Saves the planModel host values to the db then navigate to the first round vote page.
     this.model.save().then( function( response ) {
       console.log( response );
-      this.router.navigate( '/' + this.model.get('id') + '/' + round + '/' + this.model.get('currentRound'), trigger: true );
+      this.router.navigate( '/' + this.model.get( 'id' ) + '/' + round + '/' + this.model.get( 'currentRound' ), trigger: true );
     });
   },
 
+  parseInvites: function(emailString) {
+    return emailString.replace( /\s/g, '' ).split( ',' );
+  },
+
   render: function() {
-    this.$el.html( _.template( this.template )(this.model.attributes) );
+    this.$el.html( this.template( this.model.attributes ) );
     return this;
   }
 
