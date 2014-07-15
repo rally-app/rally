430e8dda40b88ae1a75b994c2454f1c6de45cd0e
jaredscheib <jared.scheib@gmail.com>
2014-07-15 13:12:44 -0700

    Implement setOptionIndices on model initialization, change priority buttons on VoteOptionsView to push index of option

--- public/scripts/models/plan-model.js
+++ public/scripts/models/plan-model.js
@@ -1,12 +1,15 @@
 window.PlanModel = Backbone.Model.extend({
-  urlRoot: "/plan/",
+  urlRoot: '/plan/',
 
   initialize: function() {
-    this.on( "reset", this.setCurrentRound.bind( this ) );
+    this.on( 'reset', function(){
+      this.setCurrentRound.call( this );
+      this.setOptionIndices.call( this );
+    }, this);
   },
 
   setCurrentRound: function() {
-    var rounds = this.get( "rounds" );
+    var rounds = this.get( 'rounds' );
     var index = (function() {
       for ( var i = 0; i < rounds.length; i++ ) {
         if ( rounds[i].winner === null ) {
@@ -15,8 +18,16 @@ window.PlanModel = Backbone.Model.extend({
       }
       return -1;
     })();
-    this.set( "currentRoundNum", index + 1 );
-    this.set( "currentRoundOptions", rounds[ index ].options );
+    this.set( 'currentRoundNum', index + 1 );
+    this.set( 'currentRoundOptions', rounds[ index ].options );
+  },
+
+  setOptionIndices: function() {
+    var currentRoundOptions = this.get( 'currentRoundOptions' );
+    for( var i = 0; i < currentRoundOptions.length ){
+      currentRoundOptions[i].set( 'index', i );
+      console.log( 'currentRoundOptions: ', currentRoundOptions[i].optionName, currentRoundOptions[i].index );
+    }
   }
 
 });
