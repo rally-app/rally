884dda6826e798c4a845ac299289e60b4f37de98
Michael Schippert <michael.schippert@gmail.com>
2014-07-16 12:16:23 -0700

    modify routes/plans.js to use reccomendations

--- routes/plan.js
+++ routes/plan.js
@@ -7,7 +7,8 @@ var router = express.Router();
 // db.find() and db.save() return promises
 // our real db implementation will have promisified methods
 var db = require( "../stubs/db" );
-var options = require( '../stubs/options-fixture' );
+// var options = require( '../stubs/options-fixture' );
+var places = require( '../modules/places.js' );
 
 router.get( "/:id", function( req, res ) {
   db.find( "plan", req.params.id )
@@ -21,13 +22,14 @@ router.get( "/:id", function( req, res ) {
 
 router.post( "/", function( req, res ) {
   var plan = req.body;
+  var query = plan.hostWhat + ' near ' + plan.hostWhere;
 
   // 1.) Issue a google request.
   // 2.) When it completes, add the results to this plan's options
   // 3.) Save the updated plan to the database.
 
   // add options to plan before save
-  plan.rounds.push( options );
+  // plan.rounds.push( options );
 
   // make google request
   // .then( results ) {
@@ -37,14 +39,33 @@ router.post( "/", function( req, res ) {
   // .then( model ) {
   //   res.send( model )
   // }
-  
-  db.save( "plan", req.body )
-  .then( function( plan ) {
-    res.send( plan );
+  places( query )
+  .then( function ( recommendations ) {
+    plan.rounds.push( {
+      options: recommendations,
+      votes: [],
+      winner: null
+    } );
+    
+    return db.save( 'plan', plan );
+  })
+  .then( function ( result ) {
+    console.log( result );
+    res.send( result );
   })
   .catch( function( statusCode ) {
     res.send( statusCode );
   });
+  
+
+  //------------------------
+  // db.save( "plan", req.body )
+  // .then( function( plan ) {
+  //   res.send( plan );
+  // })
+  // .catch( function( statusCode ) {
+  //   res.send( statusCode );
+  // });
 });
 
 module.exports = router;
