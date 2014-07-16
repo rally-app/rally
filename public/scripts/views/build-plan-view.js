99cc7b25a9659401e7f018b7bf7350e7999735f6
Michael Schippert <michael.schippert@gmail.com>
2014-07-16 15:05:00 -0700

    Modify buildPlanView to save moments when and final

--- public/scripts/views/build-plan-view.js
+++ public/scripts/views/build-plan-view.js
@@ -40,26 +40,42 @@ window.BuildPlanView = Backbone.View.extend({
        e.preventDefault();
      }
 
-    //Used to set a new date #minutes away from the current time
-    var voteEnd = function( minutes ) {
-      var date = new Date();
-      date.setMinutes( date.getMinutes() + parseInt(minutes) );
-      return date;
-    };
+    var when = this.$el.find( '[name="hostWhen"]' ).val();
+    var end = this.$el.find( '[name="finalVoteEnd"]' ).val();
+    var who = this.$el.find( '[name="hostWho"]' ).val();
 
     //Sets the planModel host values equal to the form inputs
-    this.model.set( 'hostWho', this.parseInvites( this.$el.find( '[name="hostWho"]' ).val() ) );
+    this.model.set( 'hostWho', this.parseInvites( who ) );
     this.model.set( 'hostName', this.$el.find( '[name="hostName"]' ).val() );
     this.model.set( 'hostWhat', this.$el.find( '[name="hostWhat"]' ).val() );
     this.model.set( 'hostWhere', this.$el.find( '[name="hostWhere"]' ).val() );
-    this.model.set( 'hostWhen', this.$el.find( '[name="hostWhen"]' ).val() );
-    this.model.set( 'finalVoteEnd', voteEnd( this.$el.find( '[name="finalVoteEnd"]' ).val() ) );
+    this.model.set( 'hostWhen',  this.makeWhen( when ) );
+    this.model.set( 'finalVoteEnd',  this.makeEnd( end ) );
 
     //Saves the planModel host values to the db then navigate to the first round vote page.
     var self = this;
     this.model.save().then( function( response ) {
       router.navigate( '/' + self.model.get( 'id' ) + '/round/' + self.model.get( 'currentRoundNum' ), { trigger: true } );
     });
+  },
+
+  makeWhen: function( time ) {
+    var now = moment();
+    var hr = time.slice( 0, 2 );
+    var min = time.slice( 3, 5 );
+
+    now.hours(hr);
+    now.minutes(min);
+    now.seconds(00);
+
+    return now;
+  },
+
+  makeEnd: function( minutes ) {
+    var now = moment();
+    now.add( 'minutes', minutes );
+
+    return now;
   },
 
   //Remove all spaces and split email entries into an array of emails.
