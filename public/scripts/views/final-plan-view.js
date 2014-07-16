0260f924d3090101f6cf8d7371a580c5b8d888da
jaredscheib <jared.scheib@gmail.com>
2014-07-15 19:36:23 -0700

    Add comments for eventual buttons

--- public/scripts/views/final-plan-view.js
+++ public/scripts/views/final-plan-view.js
@@ -6,7 +6,11 @@ window.FinalPlanView = Backbone.View.extend({
 
   className: 'finalPlanView',
 
-  template: hogan.compile('<span id="plan">You\'re rallying with {{voteCount}} friends tonight at {{winner}}! See you there at {{hostWhen}}!</span>'),
+  template: hogan.compile( ['<span id="plan">You\'re rallying with {{voteCount}} friends tonight at {{winner}}! See you there at {{hostWhen}}!</span>',
+              // '<div id="buttons">',
+                //buttons for map it, share it, make your own rally
+              // '</div>',
+              ].join( '\n' ) ),
 
   initialize: function(){
     this.render();
