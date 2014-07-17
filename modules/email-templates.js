85634c5c5809b85861eaddb240473b06e5853f38
Nick Bottomley <nhbottomley@gmail.com>
2014-07-17 16:48:28 -0700

    Finish basic email sending module

---
+++ modules/email-templates.js
@@ -0,0 +1,10 @@
+module.exports = {
+  round: [ 
+    "<h2>Rally</h2>",
+    "<p>Head over to {link} to vote!</p>"
+    ].join( "\n" ),
+  final: [
+    "<h2>Rally</h2>",
+    "<p>Check out the final Rally point at {link}!</p>"
+    ].join( "\n" )
+};
