581aa7015c1bfae7cf69657612f47e66dbf966ac
Nick Bottomley <nhbottomley@gmail.com>
2014-07-17 16:58:03 -0700

    Implement basic round closing logic

--- modules/close-round.js
+++ modules/close-round.js
@@ -1,7 +1,8 @@
 var calculateWinner = require( './calculate-winner' );
+var sendEmails = require( './send-emails' );
 
 module.exports = function( plan, roundNumber ) {
   var winner = calculateWinner( plan, roundNumber );
   plan.rounds[ roundNumber ].winner = winner;
-  // more stuff
-}
+  sendEmails( plan, roundNumber );
+};
