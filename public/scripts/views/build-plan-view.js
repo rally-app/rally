604cd37324de02bc5ce328a2776d8294de398db6
Nick Bottomley <nhbottomley@gmail.com>
2014-07-18 20:09:45 -0700

    Implement madlib-style UI pieces

--- public/scripts/views/build-plan-view.js
+++ public/scripts/views/build-plan-view.js
@@ -5,7 +5,7 @@ window.BuildPlanView = Backbone.View.extend({
 
   template: hogan.compile( [ '<div class="buildPlan">',
     '<p>',
-      'My name is <input type="text" name="hostName" placeholder="Full Name" value="Nicholas Henry">.',
+      'My name is <input class="madlib-input" type="text" name="hostName" placeholder="Full Name" value="Nicholas Henry">.',
     '</p>',
     '<p>',
       'I want to ',
@@ -13,9 +13,9 @@ window.BuildPlanView = Backbone.View.extend({
         '<option value="bars">drink</option>',
         '<option value="restaurants">eat</option>',
       '</select> near ',
-      '<input type="text" name="hostWhere" placeholder="location" value="the Presidio"> at ',
-      '<input type="time" name="hostWhen" value="21:30"> with ',
-      '<input type="text" name="hostWho" placeholder="emails of friends" value="Mike,Jared">.',
+      '<input class="madlib-input" type="text" name="hostWhere" placeholder="location" value="the Presidio"> at ',
+      '<input class="madlib-input" type="time" name="hostWhen" value="21:30"> with ',
+      '<input class="madlib-input" type="text" name="hostWho" placeholder="emails of friends" value="Mike,Jared">.',
     '</p>',
     '<p>',
       'Let\'s finalize this rally within the next ',
@@ -26,63 +26,54 @@ window.BuildPlanView = Backbone.View.extend({
       '</select>',
       ' minutes.',
     '</p>',
-    '<button type="button" class="createEvent">Checkmark Image</button>',
-    '<button type="reset" class="clear">Clear</button></div>' ].join("") ),
-
-
+    '<button type="button" id="createPlan">Checkmark Image</button>',
+    '<button type="reset" id="clearPlan">Clear</button></div>' ].join("") ),
 
   initialize: function() {
-    this.render();
     // save references to the inputs once so we don't have to perform mutliple $() selections
+    this.render();
+    this._inputs = this.$el.find( "input[type='text']" );
+    this._selects = this.$el.find( "select" );
     this._$hostWhen = this.$el.find( '[name="hostWhen"]' );
     this._$hostWho = this.$el.find( '[name="hostWho"]' );
     this._$hostWhat = this.$el.find( '[name="hostWhat"]' );
     this._$hostWhere = this.$el.find( '[name="hostWhere"]' );
-    this._$hostName = this.$el.find( '[name="hostName"'] );
+    this._$hostName = this.$el.find( '[name="hostName"]' );
     this._$finalVoteEnd = this.$el.find( '[name="finalVoteEnd"]' );
   },
 
   events: {
-    'click .createEvent': 'createEvent',
-    'click .clear': 'render'
+    'click #createPlan': 'createPlan',
+    'click #clearPlan': 'render'
   },
 
   getValues: function() {
     return {
-      hostWho: this._$hostWho.val(),
+      hostWho: this.parseInvites( this._$hostWho.val() ),
       hostName: this._$hostName.val(),
       hostWhat: this._$hostWhat.val(),
       hostWhere: this._$hostWhere.val(),
-      hostWhen: this._$hostWhen.val(),
-      finalVoteEnd: this._$finalVoteEnd.val()
+      hostWhen: this.makeWhen( this._$hostWhen.val() ),
+      finalVoteEnd: this.makeEnd( this._$finalVoteEnd.val() )
     };
-
-
   },
 
-  createEvent: function( e ) {
-    if( e ) {
-       e.preventDefault();
-     }
-
-    var when = this.$el.find( '[name="hostWhen"]' ).val();
-    var end = this.$el.find( '[name="finalVoteEnd"]' ).val();
-    var who = this.$el.find( '[name="hostWho"]' ).val();
-
-    //Sets the planModel host values equal to the form inputs
-    this.model.set( 'hostWho', this.parseInvites( who ) );
-    this.model.set( 'hostName', this.$el.find( '[name="hostName"]' ).val() );
-    this.model.set( 'hostWhat', this.$el.find( '[name="hostWhat"]' ).val() );
-    this.model.set( 'hostWhere', this.$el.find( '[name="hostWhere"]' ).val() );
-    this.model.set( 'hostWhen', this.makeWhen( when ) );
-    this.model.set( 'createdAt', moment().startOf( 'minute' ).add( 'minutes', 1 ) ); //round createdAt up to nearest minute for easy db use
-    this.model.set( 'finalVoteEnd', this.makeEnd( end ) );
-    this.model.set( 'attending', 1 );
+  createPlan: function( evt ) {
+    if ( evt ) {
+       evt.preventDefault();
+    }
+    var model = this.model;
+    var values = this.getValues();
+    $.extend( values, {
+      createdAt: moment().startOf( 'minute' ).add( 'minutes', 1 ),
+      attending: 1
+    });
+    model.set( values );
     
-    //Saves the planModel host values to the db then navigate to the first round vote page.
-    var self = this;
-    this.model.save().then( function( response ) {
-      router.navigate( '/' + self.model.get( 'id' ) + '/round/' + self.model.get( 'currentRoundNum' ), { trigger: true } );
+    model.save().then( function( response ) {
+      router.navigate( '/' + model.get( 'id' ) + '/round/' + model.get( 'currentRoundNum' ), 
+        { trigger: true } 
+      );
     });
   },
 
@@ -108,8 +99,18 @@ window.BuildPlanView = Backbone.View.extend({
     return emailString.replace( /\s/g, '' ).split( ',' );
   },
 
+  madlib: function() {
+    this._selects.map( function() {
+      $( this ).madlibSelect();
+    });
+    this._inputs.map( function() {
+      $( this ).madlibInput();
+    });
+  },
+
   render: function() {
     this.$el.html( this.template.render( this.model.attributes ) );
+    setTimeout( this.madlib.bind( this ), 0 );
     return this;
   }
 
