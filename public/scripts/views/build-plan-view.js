6e230381df2d4755693dd26d11a83ea6733a2f6f
Nick Bottomley <nhbottomley@gmail.com>
2014-07-21 13:01:46 -0700

    Add button styling

--- public/scripts/views/build-plan-view.js
+++ public/scripts/views/build-plan-view.js
@@ -22,15 +22,18 @@ window.BuildPlanView = Backbone.View.extend({
       '<select name="finalVoteEnd">',
         '<option value="5">5 minutes</option>',
         '<option value="15">15 minutes</option>',
-        '<option value="30">30 minutes</option>'
+        '<option value="30">30 minutes</option>',
         '<option value="60">1 hour</option>',
         '<option value="180">3 hours</option>',
         '<option value="500">5 hours</option>',
       '</select>',
-      ' minutes.',
+      '.',
     '</p>',
-    '<button class="ladda-button" data-style="expand-right" data-spinner="black" type="button" id="createPlan">Checkmark Image</button>',
-    '<button type="reset" id="clearPlan">Clear</button></div>' ].join("") ),
+    '<div class="actions">',
+    // '<button id="clearPlan"><i class="fa</button></div>' 
+      '<button class="good xl no-padding" id="createPlan"><i class="fa fa-check-circle-o"></i></button>',
+    '</div>'
+  ].join("") ),
 
   initialize: function() {
     this.render();
