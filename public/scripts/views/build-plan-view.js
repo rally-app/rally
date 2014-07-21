ccdf223cb0bdd7bf203d5f9f3161f54ff450add2
Nick Bottomley <nhbottomley@gmail.com>
2014-07-21 12:42:49 -0700

    Fix quote

--- public/scripts/views/build-plan-view.js
+++ public/scripts/views/build-plan-view.js
@@ -25,7 +25,7 @@ window.BuildPlanView = Backbone.View.extend({
         '<option value="30">30 minutes</option>'
         '<option value="60">1 hour</option>',
         '<option value="180">3 hours</option>',
-        '<option value="500">5 hours</option>,
+        '<option value="500">5 hours</option>',
       '</select>',
       ' minutes.',
     '</p>',
