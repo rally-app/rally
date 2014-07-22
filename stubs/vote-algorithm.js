23542d79fa1e1a81be4cdc48ba63ef78e8f05aac
Michael Schippert <michael.schippert@gmail.com>
2014-07-21 19:09:15 -0700

    Remove all unused files from stub directory

--- stubs/vote-algorithm.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// just a stub. returns a random choice's index.
-module.exports = function( plan, round, votes ) {
-  var targetRound = plan.rounds[ round ];
-  return targetRound.options[ Math.floor( Math.random() * targetRound.options.length ) ];
-};
